d2b9dc0cbc5fe79a4d1e47db331974019b5adf1e
victor hugo <victor03016@gmail.com>
2019-08-14 20:59:28 +0200

    Agregando el Formulario y Listado de Productos
    Creamos los componentes para el formulario y listado de productos

--- src/Components/Productos.js
+++ src/Components/Productos.js
@@ -1,10 +1,22 @@
-import React from 'react';
-
+import React from "react";
 
 const Producto = () => {
-    return ( 
-        <div>Desde productos</div>
-     );
-}
- 
+    return (
+        <React.Fragment>
+            <h2 className="text-center my-5">Listado de Productos</h2>
+
+            <table className="table table-striped">
+                <thead className="bg-primary table-dark">
+                    <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody />
+            </table>
+        </React.Fragment>
+    );
+};
+
 export default Producto;
