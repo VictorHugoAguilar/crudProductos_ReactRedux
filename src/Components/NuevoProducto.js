3d6839b7bf057612a746e3569eb8040e68618bb0
victor hugo <victor03016@gmail.com>
2019-08-15 14:02:03 +0200

    Creando acciones para validar el formulario

--- src/Components/NuevoProducto.js
+++ src/Components/NuevoProducto.js
@@ -1,77 +1,110 @@
-import React, { useState} from 'react';
+import React, { useState } from "react";
 
 // importamos las acciones
-import { crearNuevoProductoAction } from '../actions/productosActions';
+import { crearNuevoProductoAction } from "../actions/productosActions";
+import {
+    validarFormularioAction,
+    validacionExito,
+    validacionError
+} from "../actions/validacionActions";
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 
-
-const NuevoProducto = () => {
-
+const NuevoProducto = ({history}) => {
     // creamos el state local
-    const [nombre, setNombre]  = useState('');
-    const [precio, setPrecio] = useState('');
+    const [nombre, setNombre] = useState("");
+    const [precio, setPrecio] = useState("");
 
     // Crear nuevo producto
     const dispatch = useDispatch();
-    const agregarProducto = (producto) => dispatch(crearNuevoProductoAction(producto) )
+    const agregarProducto = producto => {
+        dispatch(crearNuevoProductoAction(producto));
+    };
+    const validarFormulario = () => {
+        dispatch(validarFormularioAction());
+    };
+    const exitoValidacion = () => {
+        dispatch(validacionExito());
+    };
+    const errorValidacion = () => {
+        dispatch(validacionError());
+    };
+
+    // obtener los datos del state
+    const error = useSelector(state => state.error.error);
 
     // Añadimos el nuevo producto
-    const submitNuevoProducto = (e) => {
+    const submitNuevoProducto = e => {
         e.preventDefault();
 
-        agregarProducto({ nombre, precio})
+        validarFormulario();
 
         // validamos los datos del formulario y mostrar error
-        if (nombre.trim() === '' || precio.trim() === '') {
-            console.error('Error en la validacion')
+        if (nombre.trim() === "" || precio.trim() === "") {
+            // console.error("Error en la validacion");
+            errorValidacion();
             return;
-        }  
-        console.clear();
+        }
 
-        // si pasa la validacion 
+        // si pasa la validacion
+        exitoValidacion();
 
         // crear el nuevo producto
+        agregarProducto({ nombre, precio });
 
         // redireccionar
-    }
+        history.push('/');
+    };
 
-    return ( 
+    return (
         <div className="row justify-content-center mt-5">
             <div className="col-md-8">
                 <div className="card">
                     <div className="card-body">
-                        <h2 className="text-center mb-4 font-weight-bold ">Agregar Nuevo Libro</h2>
-                        <form onSubmit={submitNuevoProducto}> 
+                        <h2 className="text-center mb-4 font-weight-bold ">
+                            Agregar Nuevo Libro
+                        </h2>
+                        <form onSubmit={submitNuevoProducto}>
                             <div className="form-group">
                                 <label>Nombre Libro</label>
-                                <input 
-                                    type="text" 
-                                    className="form-control" 
-                                    placeholder="Nombre Libro" 
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nombre Libro"
                                     value={nombre}
-                                    onChange={e => { setNombre(e.target.value)}}
+                                    onChange={e => {
+                                        setNombre(e.target.value);
+                                    }}
                                 />
                             </div>
                             <div className="form-group">
                                 <label>Precio Libro</label>
-                                <input 
-                                    type="text" 
-                                    className="form-control" 
-                                    placeholder="Precio Libro" 
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Precio Libro"
                                     value={precio}
                                     onChange={e => setPrecio(e.target.value)}
                                 />
                             </div>
 
-                            <button type="submit" className="btn btn-primary font-weight-bold text-uppercase d-block w-100">Agregar</button>
+                            <button
+                                type="submit"
+                                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                            >
+                                Agregar
+                            </button>
                         </form>
-        
+                        {error ? (
+                            <div className="alert alert-danger text-center mt-4 font-weight-bold">
+                                Todos los campos son obligatorios
+                            </div>
+                        ) : null}
                     </div>
                 </div>
             </div>
         </div>
-     );
-}
- 
+    );
+};
+
 export default NuevoProducto;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import productosReducer from './productosReducer';
+import validacionReducer from './validacionReducer';
 
 export default combineReducers({
-    productos: productosReducer
+    productos: productosReducer,
+    error: validacionReducer
 });
+
+
